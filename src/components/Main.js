87ec4df64eb0b5702ad492a227a1e61293c81601
Limited626 <zhanlq626@163.com>
2017-03-23 19:07:45 +0800

    1111

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,9 @@ require('styles/App.css');
 
 import React from 'react';
 //获取图片的数据
-var imagesDate = require('../data/imagesData.json');
+let imagesData = require('../data/imagesData.json');
 //取出图片数据得到URL
-imagesDate = (function imageDataUrl(imageDataArr) {
+imagesData = (function imageDataUrl(imageDataArr) {
   for(var i=0;i<imageDataArr.length;i++){
     var oneImageData = imageDataArr[i];
 
@@ -14,27 +14,45 @@ imagesDate = (function imageDataUrl(imageDataArr) {
     imageDataArr[i] = oneImageData;
   }
   return imageDataArr;
-})(imagesDate);
+})(imagesData);
 
 
 class AppComponent extends React.Component {
   render() {
+    var controllerUnits = [],
+        imgFigures = [];
+    imagesData.forEach(function (value) {
+      imgFigures.push(<ImgFigure data={value}/>);
+    });
+
     return (
      <section className="stage">
 
        <section className="img-con">
-
+         {imgFigures}
        </section>
 
        <nav className="controller-nav">
-
+         {controllerUnits}
        </nav>
 
      </section>
     );
   }
 }
+var ImgFigure = React.createClass({
+    render:function () {
+      return(
+        <figure>
 
+          <img src={this.props.data.imageUrl} alt={this.props.data.title} />
+          <figcaption>
+            <h2>{this.props.data.title}</h2>
+          </figcaption>
+        </figure>
+      )
+    }
+});
 AppComponent.defaultProps = {
 };
 
